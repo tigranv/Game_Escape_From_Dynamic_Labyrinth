0825256a34d53fbf05d13088b5e18360efd351b1
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-28 17:24:01 +0400

    core

---
+++ Game Escape From Lab/Game Escape From Lab/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Escape_From_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
